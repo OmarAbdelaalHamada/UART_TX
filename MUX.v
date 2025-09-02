0a270f34ecc124a34a6980078dabc294693ff942
omar <omark0635@gmail.com>
2025-09-02 14:45:26 +0300

    editing the Mux module

--- MUX.v
+++ MUX.v
@@ -8,24 +8,14 @@ module MUX(
 
 always @(*) begin
     TX_out = 1'b1; // Default to high (idle state)
-    // Check if parity is enabled
-    if(PAR_EN) begin
-        case(mux_sel)
-            2'b00: TX_out = 1'b0; // Start bit
-            2'b01: TX_out = ser_data;  // Serial data
-            2'b10: TX_out = par_bit;   // Parity bit
-            2'b11: TX_out = 1'b1;  // Stop bit
-            default: TX_out = 1'b1;     // Default case
-        endcase
-    end else begin
-        // If parity is not enabled, only use start, serial data, and stop bits
-        case(mux_sel)
-            2'b00: TX_out = 1'b0; // Start bit
-            2'b01: TX_out = ser_data;  // Serial data
-            2'b10: TX_out = 1'b1;  // Stop bit
-            default: TX_out = 1'b1;     // Default case
-        endcase
-    end
+    case(mux_sel)
+        2'b00: TX_out = 1'b0; // Start bit
+        2'b01: TX_out = ser_data;  // Serial data
+        2'b10: TX_out = par_bit;   // Parity bit
+        2'b11: TX_out = 1'b1;  // Stop bit
+        default: TX_out = 1'b1;     // Default case
+    endcase
+    
 end
 
 endmodule
